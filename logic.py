33f94df721684163e21d7d46c5817738d84b995d
Edwin G <edwgeo@gmail.com>
2020-02-24 17:35:08 -0800

    Commented Files

--- logic.py
+++ logic.py
@@ -1,15 +1,19 @@
 # this module uses the Youtube Data API v3, particularly the PlaylistItems resource
 # https://developers.google.com/youtube/v3/docs/playlistItems
-# The playlist contains all the videos on the trending page in the same order as the trending page
+
+# The playlist accessed here contains all the videos on the trending page in the same order as the trending page
 
 import requests
 import json
 
+# function gets API key from creds.txt file (see README.md for more info)
 def getCreds(filename):
 	with open(filename) as creds:
 		# Using read because creds file should only be one line long
 		return creds.read().strip()
 
+# function executes a get request and retrieves the videos in the Playlist
+# in JSON format
 def getJson(auth):
 	params = {"part": 'contentDetails, snippet',
 		"playlistId": 'PLrEnWoR732-BHrPp_Pm8_VleD68f9s14-', 
@@ -23,6 +27,13 @@ def getJson(auth):
 	jsonResult = json.loads(response.content)
 	return jsonResult
 
+# This function calls the above functions, and parses the JSON response
+# it returns a list consisting of 3-tuples, which are formatted as such:
+
+# [(title, videoUrl, thumbnail)]
+# title = the title of the video in str format
+# videoUrl = the URL of the video in str format
+# thumbnail = the URL to the default thumbnail for the video
 def getTrending():
 	auth = getCreds("creds.txt")
 	
